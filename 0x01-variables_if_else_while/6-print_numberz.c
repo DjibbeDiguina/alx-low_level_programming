bf9451bd3266a018ee3584f7e49ae3cda44e5f42
djibbediguina <diguinafils1@gmail.com>
2023-09-10 18:59:54 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0;
+ */
+
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+putchar('0' + i);
+i++;
+}
+putchar('\n');
+return (0);
+}
