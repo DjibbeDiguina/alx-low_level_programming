ea45b3e32f3d1da946e4937b84221ebeef051e59
djibbediguina <diguinafils1@gmail.com>
2023-09-10 17:58:17 -0700

    sometimes suffer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+		putchar(al);
+	putchar('\n');
+	return (0);
+}
