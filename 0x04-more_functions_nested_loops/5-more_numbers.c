f791f757ab5e8cd86e1040446c588a26e5b57d50
djibbediguina <diguinafils1@gmail.com>
2023-09-15 19:39:20 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void more_numbers(void) {
+    int i, j;
+
+    for (j = 0; j < 10; j++) {
+        for (i = 0; i <= 14; i++) {
+            _putchar(i + '0');
+        }
+        _putchar('\n');
+    }
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void more_numbers(void);
 void print_most_numbers(void);
 #endif
